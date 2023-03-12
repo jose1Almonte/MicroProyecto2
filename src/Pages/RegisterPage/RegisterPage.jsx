6b46b0ece4ebcc3f7ba27bb136f46c7ba453be07
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-03-12 16:20:25 -0400

    Placeholders

--- src/Pages/RegisterPage/RegisterPage.jsx
+++ src/Pages/RegisterPage/RegisterPage.jsx
@@ -52,17 +52,17 @@ export function RegisterPage() {
 
         <div className={Styles.contentBetween}>
           <h1 className={Styles.whiteText}> name</h1>
-          <input type="text" className={Styles.textField} placeholder = 'name' name = 'name' onChange={handleOnChange}/>
+          <input type="text" className={Styles.textField} placeholder = 'Tu nombre y apellido' name = 'name' onChange={handleOnChange}/>
         </div>
 
         <div className={Styles.contentBetween}>
           <h1 className={Styles.whiteText}>Email</h1>
-          <input type="text" className={Styles.textField} placeholder = 'email' name = 'email' onChange={handleOnChange}/>
+          <input type="text" className={Styles.textField} placeholder = 'Ej. CarteleraCaracas@email.com' name = 'email' onChange={handleOnChange}/>
         </div>
         
         <div className={Styles.contentBetween}>
           <h1 className={Styles.whiteText}>Password</h1>
-          <input type="password" className={Styles.textField} placeholder = 'password' name = 'password' onChange={handleOnChange}/>
+          <input type="password" className={Styles.textField} placeholder = 'Contraseña' name = 'password' onChange={handleOnChange}/>
         </div>
 
         <div className={Styles.content}>
