390b5fe40d890486987408204cbe89ac2a6da418
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2023-03-12 09:37:37 -0400

    spaces (not important)

--- src/components/DetailsCard/DetailsCard.jsx
+++ src/components/DetailsCard/DetailsCard.jsx
@@ -7,35 +7,51 @@ function DetailsCard({ Movie }) {
     
     return (
         <div className={Styles.base}>
+
             <div className={Styles.title}>
+
                 <h1>{title}</h1>
+
             </div>
+
             <div className={Styles.info}>
+
                 <div className={Styles.poster}>
                     <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={ `Poster de ${title}`} />
                 </div>
+
                 <div className={Styles.text}>
+
                     <div className={Styles.data}>
+                        
                         <div className={Styles.genres}>
                             {genres.map((genre) => (
                                 <h3>- {genre.name} -</h3>
                             ))}
                         </div>
+
                         <h3>Lenguaje original: {original_language}</h3>
                         <h3>Presupuesto: {budget}</h3>
                         <h3>Descripcion: {overview}</h3>
                         <h3>Popularidad: {popularity}</h3>
+                        
                         <div>
                             <h3>Compañias: </h3>
                             {production_companies.map((company) => (
                                 <h3>- { company.name }</h3>
                             ))}
                         </div>
+
                         <h3>Fecha de lanzamiento: {release_date}</h3>
+
                         <h3>Estado: {status} </h3>
+
                     </div>
+
                 </div>
+
             </div>
+            
         </div>
     )
 }
